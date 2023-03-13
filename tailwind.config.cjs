1db9445b8608988582a771a822dedbc25418bceb
srinidharan <srinidharan2001@gmail.com>
2023-03-13 14:00:20 +0000

    Initial design modified

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
+	daisyui: {
+		themes: [
+		  {
+			mytheme: {
+			
+   "primary": "#C71F37",
+			
+   "secondary": "#C71F37",
+			
+   "accent": "#7de2d1",
+			
+   "neutral": "#C71F37",
+			
+   "base-100": "#313131",
+			
+   "info": "#3ABFF8",
+			
+   "success": "#36D399",
+			
+   "warning": "#fde047",
+			
+   "error": "#dc2626",
+			},
+		  },
+		],
+	  },
 	plugins: [require("@tailwindcss/typography"),require("daisyui")],
 }
